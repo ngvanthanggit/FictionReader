99d4cc2f66374024901df312dc2b76f84b74060a
ngvanthanggit <thangitcbg@gmail.com>
2024-07-04 23:59:18 +0700

    Initialize Login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     path("login/", views.login_view, name="accounts_login"),
     path("logout/", views.logout_view, name="accounts_logout"),
-    path("session/", views.session_view, name="accounts_session"),
-    path("whoami/", views.whoami_view, name="accounts_whoami"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,43 +1,73 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 import json
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.http import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.http import require_POST
 
+from .models import User
 
-@require_POST
 def login_view(request):
-    data = json.loads(request.body)
-    username = data.get("login_username")
-    password = data.get("login_password")
+    if request.user.is_authenticated:
+        return redirect('home')
     
-    if username == "":
-        print(username, password)
-    
-    if username is None or password is None:
-        return JsonResponse({"detail": "Please provide username and password"})
-    user = authenticate(username=username, password=password)
-    if user is None:
-        return JsonResponse({"detail": "Invalid credentials"},
-                            status=400)
-    login(request, user)
-    return JsonResponse({"detail": "Successfully logged in!"})
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages(request, 'User does not exist')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages(request, 'Username or Password is wrong!')
+        
+    context = {}
+    return render(request, 'accounts/login_page.html')
 
 def logout_view(request):
-    if not request.user.is_authenticated:
-        return JsonResponse({"detail": "You are not logged in"}, status=400)
-    logout(request)
-    return JsonResponse({"detail": "Successfully logged out!"})
+    context = {}
+    return render(request, 'home.html', context)
 
-@ensure_csrf_cookie
-def session_view(request):
-    if not request.user.is_authenticated:
-        return JsonResponse({"isauthenticated": False})
-    return JsonResponse({"isauthenticated": True})
 
-def whoami_view(request):
-    if not request.user.is_authenticated:
-        return JsonResponse({"isauthenticated": False})
-    return JsonResponse({"username": request.user.username})
+# @require_POST
+# def login_view(request):
+#     data = json.loads(request.body)
+#     username = data.get("login_username")
+#     password = data.get("login_password")
+    
+#     if username == "":
+#         print(username, password)
+    
+#     if username is None or password is None:
+#         return JsonResponse({"detail": "Please provide username and password"})
+#     user = authenticate(username=username, password=password)
+#     if user is None:
+#         return JsonResponse({"detail": "Invalid credentials"},
+#                             status=400)
+#     login(request, user)
+#     return JsonResponse({"detail": "Successfully logged in!"})
+
+# def logout_view(request):
+#     if not request.user.is_authenticated:
+#         return JsonResponse({"detail": "You are not logged in"}, status=400)
+#     logout(request)
+#     return JsonResponse({"detail": "Successfully logged out!"})
+
+# @ensure_csrf_cookie
+# def session_view(request):
+#     if not request.user.is_authenticated:
+#         return JsonResponse({"isauthenticated": False})
+#     return JsonResponse({"isauthenticated": True})
+
+# def whoami_view(request):
+#     if not request.user.is_authenticated:
+#         return JsonResponse({"isauthenticated": False})
+#     return JsonResponse({"username": request.user.username})
